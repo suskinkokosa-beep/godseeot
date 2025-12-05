0f903ae792248f653642ef1b0e92f764f5c42944
suskinkokosa-beep <suskinkokosa@gmail.com>
2025-12-05 17:54:23 +0300

    Add files via upload

--- godot_client_3d/src/core/constants.gd
+++ godot_client_3d/src/core/constants.gd
@@ -1,12 +1,10 @@
-extends Resource
+extends Node
 
 ##
 ## Global gameplay / project constants for Isleborn Online client.
 ## In early prototype this is mostly a placeholder so that higher–level
 ## systems already have a central place to import shared values.
 ##
-
-# Removed class_name to avoid autoload conflict
 
 const PROJECT_NAME := "Isleborn Online"
 const VERSION := "0.1.0"
@@ -19,5 +17,19 @@ const TIER_T2 := 2
 const TIER_T3 := 3
 const TIER_T4 := 4
 const TIER_T5 := 5
+
+var GATEWAY_WS_URL := "ws://localhost:8090"
+var ISLAND_SERVICE_URL := "http://localhost:5001"
+var WEB_FRONTEND_URL := "http://localhost:5000"
+
+func _ready() -> void:
+        var replit_domain := OS.get_environment("REPLIT_DOMAINS")
+        if replit_domain != "":
+                GATEWAY_WS_URL = "wss://" + replit_domain.replace("-00-", "-8090-00-")
+                ISLAND_SERVICE_URL = "https://" + replit_domain.replace("-00-", "-5001-00-")
+                WEB_FRONTEND_URL = "https://" + replit_domain
+        print("[Constants] Isleborn Online v", VERSION)
+        print("[Constants] Gateway WS: ", GATEWAY_WS_URL)
+        print("[Constants] Island Service: ", ISLAND_SERVICE_URL)
 
 
