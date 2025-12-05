0765f2305303d6ba2e0965dd601c0ea28f4cc5b6
suskinkokosa-beep <suskinkokosa@gmail.com>
2025-12-05 16:15:02 +0300

    Add files via upload

---
+++ godot_client_3d/assets/models/buildings/harbor/game/art/shapes/FreeHarborProps/Warf_01/materials.cs
@@ -0,0 +1,35 @@
+
+singleton Material(Warf_01_WarfTop)
+{
+   mapTo = "WarfTop";
+   diffuseMap[0] = "conc01";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "55";
+   translucentBlendOp = "None";
+   normalMap[0] = "conc01_NRM.png";
+   pixelSpecular[0] = "1";
+   useAnisotropic[0] = "1";
+};
+
+singleton Material(Warf_01_WarfWood)
+{
+   mapTo = "WarfWood";
+   diffuseMap[0] = "3td_MossWood_04";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+   normalMap[0] = "3td_MossWood_04_NRM.png";
+   pixelSpecular[0] = "1";
+   useAnisotropic[0] = "1";
+};
+
+singleton Material(Warf_01_WarfSides)
+{
+   mapTo = "WarfSides";
+   diffuseMap[0] = "3td_Bulkhead_01";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "3td_Bulkhead_01_NRM.png";
+   useAnisotropic[0] = "1";
+};
